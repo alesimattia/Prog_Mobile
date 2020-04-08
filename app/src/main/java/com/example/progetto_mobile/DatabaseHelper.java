4fd1b67d9d9a97acad583c183df509a079e191bb
alesimattia <alesimattia@gmail.com>
2020-04-08 12:22:29 +0200

    Firebase insatlled

--- app/src/main/java/com/example/progetto_mobile/DatabaseHelper.java
+++ app/src/main/java/com/example/progetto_mobile/DatabaseHelper.java
@@ -3,7 +3,7 @@ package com.example.progetto_mobile;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Tratta.db";
