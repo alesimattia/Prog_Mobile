093a008f0df2bfbf9c5e0394fb5d6b8a3ecf26b5
alesimattia <alesimattia@gmail.com>
2020-04-09 19:34:05 +0200

    Upload

--- app/src/main/java/com/example/progetto_mobile/MainActivity.java
+++ app/src/main/java/com/example/progetto_mobile/MainActivity.java
@@ -3,135 +3,86 @@ package com.example.progetto_mobile;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button registrazione;
-    private Button logIn;
-    private CustomDIalog customDialog;
+    private Button registraBtn;
+    private Button loginBtn;
+    private RegisterActivity customDialog;
     private Context context1;
-    private EditText editText_email1;
-    private EditText editText_password1;
+    private EditText emailText;
+    private EditText passwordText;
     public String email = null;
     public String password = null;
-    private int j;
-
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
 
-        registrazione = findViewById(R.id.Registrati);
-        logIn = findViewById(R.id.login);
-        editText_email1 = findViewById(R.id.editText_email1);
-        editText_password1 = findViewById(R.id.editText_password1);
+        mAuth=FirebaseAuth.getInstance();
 
+        registraBtn = findViewById(R.id.registerBtn);
+        loginBtn = findViewById(R.id.loginBtn);
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
 
-        registrazione.setOnClickListener(this);
-        logIn.setOnClickListener(this);
+        registraBtn.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
     }
 
 
 
 
-
-private void Log_In(){
-    HttpURLConnection client = null;
-
-    URL url;
-    try{
-
-        url = new URL("http://unicar.altervista.org/login_andrea.php?email=" + this.email + "&password=" +this.password);
-                client = (HttpURLConnection) url.openConnection();
-
-        client.setRequestMethod("GET");
-        client.setDoInput(true);
-        InputStream in = client.getInputStream();
-        String json_string = ReadResponse.readStream(in).trim();
-        JSONObject json_data = convert2JSON(json_string);
-
-
-        if (json_string.equals("1")) {
-            Toast.makeText(this, "LogIn effettuato", Toast.LENGTH_LONG).show();
-            j=1;
-            if(j==1){
-                Intent i = new Intent(this, Drive_Pass.class );
-
-                Bundle bundle = new Bundle();
-                bundle.putString("emb", email);
-                bundle.putString("psb", password);
-                i.putExtra("loginb", bundle);
-
-                startActivity(i);}
-        } else {
-            Toast.makeText(this, "Errore nell'inserimento", Toast.LENGTH_LONG).show();
-            j=0;
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    finally{
-        if (client!= null){
-            client.disconnect();
-        }
+    public void login(String email,String password){
 
     }
-}
 
-    private JSONObject convert2JSON(String json_data){
-        JSONObject obj = null;
-        try {
-            obj = new JSONObject(json_data);
-            Log.d("My App", obj.toString());
-        } catch (Throwable t) {
-            Log.e("My App", "Could not parse malformed JSON: \"" + json_data + "\"");
-        }
-        return obj;
-    }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginBtn: {
+                try {
+                    email = emailText.getText().toString();
+                    password = passwordText.getText().toString();
+                } catch (NullPointerException e) {   //nel caso di campi vuoti
+                    Toast.makeText(MainActivity.this, getString(R.string.emptyField), Toast.LENGTH_LONG).show();
+                }
 
-
-
-
-        switch(v.getId()){
-            case R.id.Registrati:
-                customDialog = new CustomDIalog(this, true);
-                customDialog.show();
+                login(email, password);
                 break;
-
-                case R.id.login:
-
-
-                    email = editText_email1.getText().toString();
-                    password = editText_password1.getText().toString();
-                    Log_In();
-                    break;
+            }
+            case R.id.registerBtn: {
+                Intent regIntent=new Intent(this, RegisterActivity.class);
+                startActivity(regIntent);
+                break;
+            }
+            default:    //per protezione
+                Toast.makeText(MainActivity.this, "Non permesso", Toast.LENGTH_LONG).show();
         }
-
-
     }
+
 
 }
 
--- app/src/main/java/com/example/progetto_mobile/RegisterActivity.java
+++ app/src/main/java/com/example/progetto_mobile/RegisterActivity.java
@@ -2,29 +2,43 @@ package com.example.progetto_mobile;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class CustomDIalog extends Dialog implements View.OnClickListener {
-    private EditText editText_email;
-    private EditText editText_password;
+public class RegisterActivity extends Dialog implements View.OnClickListener {
+
     private EditText editText_nome;
     private EditText editText_cognome;
+    private EditText editText_data;
+    private EditText editText_email;
+    private EditText editText_password;
     private EditText editText_matricola;
     private EditText editText_patente;
-    private EditText editText_data;
     private EditText editText_numero;
 
-    private Button cancelButton;
-    private Button inserisciButton;
+    private Button cancelBtn;
+    private Button registerBtn;
 
     public String email = null;
     public String password = null;
@@ -32,21 +46,20 @@ public class CustomDIalog extends Dialog implements View.OnClickListener {
     public String cognome = null;
     public String matricola = null;
     public String patente = null;
-    public String data = null;
+    public Date data = null;
     public String numero = null;
-    private Context context;
-    private Boolean typeRequest;
 
-    public CustomDIalog(Context context, Boolean type) {
-        super(context);
-        this.context = context;
-        this.typeRequest = type;
-    }
+    private FirebaseAuth mAuth;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super(context);
+        this.context = context;
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_layout);
-        setTitle("Aggiungi utente");
+        setContentView(R.layout.register_layout);
+        setTitle(context.getResources().getString(R.string.registerTitle));   //OPPURE this.getContext().
 
         editText_email = findViewById(R.id.editText_email);
         editText_password = findViewById(R.id.editText_password);
@@ -57,28 +70,44 @@ public class CustomDIalog extends Dialog implements View.OnClickListener {
         editText_data = findViewById(R.id.editText_data);
         editText_numero = findViewById(R.id.editText_num);
 
-        cancelButton = findViewById(R.id.dialog_cancella);
-        inserisciButton = findViewById(R.id.dialog_inserisci);
-        cancelButton.setOnClickListener(this);
-        inserisciButton.setOnClickListener(this);
+        cancelBtn = findViewById(R.id.cancelBtn);
+        registerBtn = findViewById(R.id.registerBtn);
+        cancelBtn.setOnClickListener(this);
+        registerBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
-            case R.id.dialog_inserisci:
+            case R.id.registerBtn:
+
                 email = editText_email.getText().toString();
                 password = editText_password.getText().toString();
                 nome = editText_nome.getText().toString();
                 cognome = editText_cognome.getText().toString();
                 matricola = editText_matricola.getText().toString();
                 patente = editText_patente.getText().toString();
-                data = editText_data.getText().toString();
-                numero = editText_numero.getText().toString();
+                data = editText_data.getText().toString();      //VERIFICA con formato DATE
+                //numero = editText_numero.getText().toString();
 
+                List<String> data = new ArrayList<String>();
+                data.add(email);
+                data.add(password);
+                data.add(nome);
+                data.add(cognome);
+                data.add(matricola);
+                data.add(data);
+                //data.add(numero);
 
+                for(final String current: data)
+                    if(current.isEmpty()){
+                        toString(current).setError(context.getResources().getString(R.string.emptyField2));)
+                        toString(current).requestFocus();
+                        return;
+                    }
+                /*
                if(nome.isEmpty()){
-                    editText_nome.setError("dati da inserire");
+                    editText_nome.setError(context.getResources().getString(R.string.emptyField2));   //VERIFICA SE BASTA getString(...)
                     editText_nome.requestFocus();
                     return;
                 }
@@ -122,44 +151,30 @@ public class CustomDIalog extends Dialog implements View.OnClickListener {
                     editText_numero.requestFocus();
                     return;
                 }
-
-                make_request();
+                */
+                registration();
                 dismiss();
                 break;
 
-            case R.id.dialog_cancella:
+            case R.id.cancelBtn:
                 dismiss();
+                Intent intent=new Intent(this, MainActivity.class);
                 break;
         }
     }
 
-    private void make_request(){
-        HttpURLConnection client = null;
-        URL url;
-        try {
-            // se la richiesta è GET
-            if (this.typeRequest) {
-                url = new URL("http://unicar.altervista.org/aggiungi_utente.php?nome=" + this.nome + "&cognome=" + this.cognome + "&datadinascita=" + this.data + "&email=" + this.email
-                        + "&password=" + this.password +  "&matricola=" +this.matricola
-                + "&codicepatente=" + this.patente + "&numerotelefono=" + this.numero);
-                client = (HttpURLConnection) url.openConnection();
-                client.setRequestMethod("GET");
-                client.setDoInput(true);}
-
-            InputStream in = client.getInputStream();
-            String json_string = ReadResponse.readStream(in).trim();
-
-
-            if (json_string.equals("1")) {
-                Toast.makeText(this.context, "Inserimento effettuato", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this.context, "Errore nell'inserimento", Toast.LENGTH_LONG).show();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (client != null) {
-                client.disconnect();
+    private void registration(String email, String password, final String nome, String matricola, String patente, Date data) {
+        if(password.length()<=6) {
+            Toast.makeText(RegisterActivity.this, context.getString(R.string.passAlert),Toast.LENGTH_SHORT).show();
+        }
+        else mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() { //operazione asincrona
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    FirebaseUser user=mAuth.getCurrentUser();
+                    //AGGIUNGERE DATI UTENTE    esempio: collection Utente(email, password, nome, cognome...)
+                }
+                else Toast.makeText(RegisterActivity.this, context.getString(R.string.errorSignup),Toast.LENGTH_SHORT).show();
             }
         }
     }
