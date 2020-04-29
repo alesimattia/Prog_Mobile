35221a3ff6eca0c65048dfb8ab1101f0df266241
alesimattia <alesimattia@gmail.com>
2020-04-29 21:49:27 +0200

    ShowRides con data
    Vengono mostrate solo le corse la cui data è futura.
    Le corse passate non vengono mostrate ma rimangono salvate.

--- app/src/main/java/com/example/progetto_mobile/ShowRides.java
+++ app/src/main/java/com/example/progetto_mobile/ShowRides.java
@@ -5,33 +5,26 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import org.w3c.dom.Text;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ShowRides extends AppCompatActivity {
 
@@ -41,6 +34,7 @@ public class ShowRides extends AppCompatActivity {
     private RideAdapter rideAdapter;
     private ArrayList<Ride> corse = new ArrayList<>();
     private View element;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,38 +50,53 @@ public class ShowRides extends AppCompatActivity {
         rideAdapter = new RideAdapter(corse);
         recyclerView.setAdapter(rideAdapter);
 
-
         CollectionReference docRef = db.collection("viaggi");
         docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    for (final DocumentSnapshot document : task.getResult().getDocuments()) {
-
-                        Ride ride=new Ride(document.get("tratta").toString(),
-                                getString(R.string.direction)+"\t\t"+document.get("verso").toString(),
-                                getString(R.string.seats)+"\t\t"+document.get("posti").toString(),
-                                document.get("ora").toString(),
-                                document.get("data").toString());
-                        corse.add(ride);
-                        rideAdapter.notifyDataSetChanged();    //aggiorna la recyclerView con le modifiche effettuate alla struttura
-
-                        View inflatedView = getLayoutInflater().inflate(R.layout.ride, null);
-                        Button obj = inflatedView.findViewById(R.id.contact);
-                        obj.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Intent intent = new Intent(Intent.ACTION_DIAL);
-                                intent.setData(Uri.parse("tel:"+document.get("telefono").toString()));
-                                startActivity(intent);
-                            }
-                        });
-                    }
+                    for (final DocumentSnapshot document : task.getResult().getDocuments())
+                        putDocument(document);
                 }
                 else Log.w("tag", "errore accesso db");
             }
         });
     }
 
+
+
+    private void putDocument(final DocumentSnapshot document){
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");   //mm minuscolo corrisponde a "minute"
+        Date current = Calendar.getInstance().getTime();
+        try {
+            Date data = format.parse(document.get("data").toString());
+            if (data.after(current)) {
+                Ride ride = new Ride(document.get("tratta").toString(),
+                        getString(R.string.direction) + "\t\t" + document.get("verso").toString(),
+                        getString(R.string.seats) + "\t\t" + document.get("posti").toString(),
+                        document.get("ora").toString(),
+                        document.get("data").toString());
+                corse.add(ride);
+                rideAdapter.notifyDataSetChanged();    //aggiorna la recyclerView con le modifiche effettuate alla struttura
+
+                /**NON FUNZIONA**/
+                View inflatedView = getLayoutInflater().inflate(R.layout.ride, null);
+                Button obj = inflatedView.findViewById(R.id.contact);
+                obj.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_DIAL);
+                        intent.setData(Uri.parse("tel:" + document.get("telefono").toString()));
+                        startActivity(intent);
+                    }
+                });
+            }
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+            Log.w("tag", "errore lettura data");
+        }
+    }
 }
